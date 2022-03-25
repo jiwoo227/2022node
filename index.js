65e43f387fde38cfed1ae53b8c180b1c3c76f052
jiwoo227 <w2040@e-mirim.hs.kr>
2022-03-25 14:46:36 +0900

    web2 -5 정적서버페이지

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require("fs");
+
+const app = http.createServer(function (req, res){
+    let url = req.url;
+
+    if (url === '/')
+        url = '/index.html';
+    if (url === '/favicon.ico')
+        return res.writeHead(404);
+        res.end(fs.readFileSync(__dirname+ url));
+
+    res.writeHead(200);
+    console.log(__dirname + req.url);
+    res.end("hello world");
+
+});
+app.listen(3333);
