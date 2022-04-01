334ac082188f4979d45e52814a433e5d9c2cef77
jiwoo227 <w2040@e-mirim.hs.kr>
2022-04-01 14:09:00 +0900

    web 2-23 파일 목록

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = http.createServer(function (request, response) {
 <head>
 <title>WEB1 - ${title}</title>
 <meta charset="utf-8">
-</head>
+</head> 
 <body>
 <h1><a href="/">WEB</a></h1>
 <ul>
@@ -63,4 +63,4 @@ const app = http.createServer(function (request, response) {
         response.end('Not found')
     }
 })
-app.listen(3000)
+app.listen(3333)
