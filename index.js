5d8c11becad6921e27d9859b0d6e1fce00f707f0
jiwoo227 <w2040@e-mirim.hs.kr>
2022-04-15 13:32:36 +0900

    4-8 수업

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
 function templateList(filelist){
     let list = '<ul>';
@@ -10,8 +11,7 @@ function templateList(filelist){
     list += '</ul>';
     return list;
 }
-
-function templateHTML(title, list, body){
+function templateHTML(title, list, body,control){
     return `
           <!doctype html>
           <html lang="ko">
@@ -23,12 +23,12 @@ function templateHTML(title, list, body){
             <h1><a href="/">WEB</a></h1>
             ${list}
             <h2>${title}</h2>
+            ${control}
             <p>${body}</p>
           </body>
           </html>
           `
 }
-
 const app = http.createServer(function (request, response) {
     const _url = request.url
     const queryData = url.parse(_url, true).query
@@ -37,26 +37,56 @@ const app = http.createServer(function (request, response) {
         if (queryData.id === undefined) {
             const title = 'Welcome'
             const description = 'Hello, Node.js'
+
             fs.readdir('data/', function (err, data){
                 const list = templateList(data);
-                const template = templateHTML(title, list, description);
+                //메인화면에서는 create(새 게시글 작성)만 가능하게
+                const template = templateHTML(title, list, description,'<a href="create">create</a>');
                 response.writeHead(200)
                 response.end(template)
             })
-        } else {
+        }else {
             fs.readdir('data/', function (err, data){
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                    const title = queryData.id;
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description){
+                    const title = queryData.id
                     const list = templateList(data);
-                    const template = templateHTML(title, list, description);
+                    const template = templateHTML(title,list,description,'<a href="create">create</a><a href="update?id=${title}">update</a>');
                     response.writeHead(200)
                     response.end(template)
                 })
             });
         }
+    }else if(pathname === '/create'){
+        fs.readdir('data/',function (err,data){
+            const title = 'Web - create';
+            const list = templateList(data);
+            const template = templateHTML(title, list,`
+          <form action="create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p><textarea name="description" placeholder="description"></textarea></p>
+              <p><input type="submit"></p>
+          </form>`, '') //글 생성 중에는 create가 안보이게
+            response.writeHead(200);
+            response.end(template)
+        });
+    }
+    else if(pathname === '/create_process'){
+        let body = '';
+        request.on('data',function (data){
+            body += body + data;
+        });
+        request.on('end',function (){
+            const post = qs.parse(body);
+            const title = post.title;
+            const description = post.description;
+            fs.writeFile(`data/${title}`, description,"utf-8", function (err){
+                response.writeHead(302,{Location:`/?id=${title}`});
+                response.end();
+            })
+        });
     } else {
         response.writeHead(404)
         response.end('Not found')
     }
 })
-app.listen(3333)
+app.listen(3334)
