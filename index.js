efa7a0b40cdc62df276da393b83dcae084cfb4ef
jiwoo227 <w2040@e-mirim.hs.kr>
2022-05-30 14:01:30 +0900

    홈페이지 구현 완벽

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const template = require('./lib/template.js');
 const fs = require('fs');
+const qs = require("querystring");
 
 app.get('/', function (req, res) {
     fs.readdir('./data', function(err, filelist){
@@ -18,7 +19,114 @@ app.get('/', function (req, res) {
 
 
 app.get('/page/:pageId', function (req, res) {
-    res.send(req.params);
+    fs.readdir('./data', function(err, filelist){
+        const list = template.List(filelist);
+        const id = req.params.pageId;
+        fs.readFile(`./data/${id}`, 'utf8', function (error, description){
+            const title = id;
+            const html = template.HTML(title, list, description
+                , `<a href="/create">create</a>
+                <a href="/update/${title}">update</a> 
+                <form action="/delete_process" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <input type="submit" value="delete">
+                </form>`);
+            res.send(html);
+        })
+    })
 })
 
-app.listen(3333);
+
+app.get('/create', function (req, res){
+    fs.readdir('./data', function (err, filelist) {
+        const title = 'WEB - create';
+        const list = template.List(filelist);
+        const html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
+        res.send(html);
+    });
+});
+
+app.post('/create_process', function(req, res){
+    let body = '';
+    req.on('data', function (data) {
+        body = body + data;
+    });
+    req.on('end', function () {
+        const post = qs.parse(body);
+        const title = post.title;
+        const description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+            res.redirect(`/page/${title}`);
+        });
+    });
+});
+
+app.get('/update/:pageId', function(req, res){
+    fs.readdir('./data', function (err, filelist) {
+        const id = req.params.pageId;
+        fs.readFile(`data/${id}`, 'utf8', function (error, description) {
+            const title = id;
+            const list = template.List(filelist);
+            const html = template.HTML(title, list,
+                `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+                `<a href="/create">create</a> <a href="/update/${title}">update</a>`,
+            );
+            res.send(html);
+        });
+    });
+});
+
+app.post('/update_process', function (req, res) {
+    let body = '';
+    req.on('data', function (data) {
+        body = body + data;
+    });
+    req.on('end', function () {
+        const post = qs.parse(body);
+        const id = post.id;
+        const title = post.title;
+        const description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function (error) {
+            fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                res.redirect(`/page/${title}`);
+            });
+        });
+    });
+});
+
+app.post('/delete_process', function(req, res){
+    let body = '';
+    req.on('data', function (data) {
+        body = body + data;
+    });
+    req.on('end', function () {
+        const post = qs.parse(body);
+        const id = post.id;
+        fs.unlink(`data/${id}`, function (error) {
+            res.redirect(`/`);
+        });
+    });
+});
+
+app.listen(4444);
